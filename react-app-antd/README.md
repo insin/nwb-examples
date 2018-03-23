eeadf4b56bdfc983a3c6fb71a933c6793fd460cc
Jonny Buchanan <jonathan.buchanan@gmail.com>
2018-03-24 03:30:42 +1000

    Add Less + variable modification config to the react-app-antd example
    insin/nwb#443

--- react-app-antd/README.md
+++ react-app-antd/README.md
@@ -1,3 +1,4 @@
-Example of configuring a `react-app` project for [Ant Design](https://ant.design/).
+Examples of configuring a `react-app` project for [Ant Design](https://ant.design/)
+for regular CSS and for Less with customised theme variables.
 
 See the [Ant Design Use in create-react-app](https://ant.design/docs/react/use-with-create-react-app) docs.
--- react-app-antd/nwb.config.js
+++ react-app-antd/nwb.config.js
@@ -1,8 +1,29 @@
+// Regular CSS config
+// module.exports = {
+//   type: 'react-app',
+//   babel: {
+//     plugins: [
+//       ['import', {libraryName: 'antd', libraryDirectory: 'es', style: 'css'}]
+//     ]
+//   }
+// }
+
+// Less config (with nwb-less installed)
 module.exports = {
   type: 'react-app',
   babel: {
     plugins: [
-      ['import', {libraryName: 'antd', libraryDirectory: 'es', style: 'css'}]
+      ['import', {libraryName: 'antd', libraryDirectory: 'es', style: true}]
     ]
+  },
+  webpack: {
+    rules: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          '@primary-color': '#1DA57A'
+        }
+      }
+    }
   }
 }

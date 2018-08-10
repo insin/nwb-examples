84e326ff54d513b6663862f66777e7d10d81fdad
Jonny Buchanan <jonathan.buchanan@gmail.com>
2018-08-10 19:03:22 +1000

    Add importing-fonts example

---
+++ importing-fonts/README.md
@@ -0,0 +1,1 @@
+Example of importing a font from CSS.
---
+++ importing-fonts/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+import React, {Component} from 'react'
+
+class App extends Component {
+  render() {
+    return <div className="App">
+      Playing Fair, Displaying Bold
+    </div>
+  }
+}
+
+export default App
